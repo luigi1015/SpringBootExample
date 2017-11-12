8f52033c4a9eb5f31640c011dd4161550b15f4bc
Jeff <github@314ghz.com>
2017-11-12 08:02:49 -0600

    Added a bean and a list of the beans.

---
+++ SpringBootExample/src/main/java/com/codehobby/springbootexample/HelloController.java
@@ -0,0 +1,15 @@
+package com.codehobby.springbootexample;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController
+{
+	@RequestMapping("/")
+	public String hello()
+	{
+		return "Hello, World!";
+	}
+
+}
